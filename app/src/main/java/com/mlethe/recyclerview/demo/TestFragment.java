c28b2e43ecf2de4c6fd899facd4e477898bd347e
Mlethe <mlethe@sina.com>
2019-08-02 10:20:03 +0800

    TestFragment修改

--- app/src/main/java/com/mlethe/recyclerview/demo/TestFragment.java
+++ app/src/main/java/com/mlethe/recyclerview/demo/TestFragment.java
@@ -4,6 +4,7 @@ package com.mlethe.recyclerview.demo;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -35,8 +36,13 @@ public class TestFragment extends Fragment implements MRecyclerView.OnRefreshLis
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_test, container, false);
-        recyclerView = view.findViewById(R.id.test_mrv);
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView = getView().findViewById(R.id.test_mrv);
         adapter = new RecyclerViewAdapter(getContext(), dataList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.addItemDecoration(new LinearItemDecoration(getContext(), R.drawable.line_1dp));
@@ -44,7 +50,6 @@ public class TestFragment extends Fragment implements MRecyclerView.OnRefreshLis
                 .setOnLoadListener(this)
                 .setAdapter(adapter);
         recyclerView.refresh();
-        return view;
     }
 
     @Override
